b903dc6444b92e4ac080174229c4b283d9d88676
vyshnav <me@example.com>
2025-04-15 16:42:43 +0530

    init commit

---
+++ SD-landingpage.js
@@ -0,0 +1,11 @@
+function redirect(){
+    window.open('http://127.0.0.1:5500/SD-login%20page.html', '_blank');
+}
+
+function alert_func(){
+    alert("sign up successful")
+
+}
+
+
+
---
+++ SD-loginpage.js
@@ -0,0 +1,3 @@
+function redirect(){
+    window.open('http://127.0.0.1:5500/writing-sec.html','_blank');
+}
